53cab491d39c9262cab13795ed2b85498abb5beb
Sefa Eyeoglu <contact@scrumplex.net>
2024-01-01 15:10:13 +0100

    chore: initial commit
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nixpkgs overlay for bleeding-edge XR/AR/VR packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [nvfetcher];
+        };
+      };
+      flake.overlays.default = final: prev:
+        let
+          inherit (builtins) attrNames;
+          inherit (nixpkgs.lib) genAttrs;
+
+          sources = final.callPackage ./_sources/generated.nix {};
+        in
+          genAttrs (attrNames sources) (pkg: prev.${pkg}.overrideAttrs (_: sources.${pkg}));
+    };
+}

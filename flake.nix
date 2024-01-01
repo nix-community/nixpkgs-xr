11a132e57cf226d36e5ced91a288bfadd465eb0e
seth <getchoo@tuta.io>
2024-01-01 14:17:14 -0500

    refactor!: drop flake-parts

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2024 Sefa Eyeoglu <contact@scrumplex.net>
 #
 # SPDX-License-Identifier: MIT
-
 {
   description = "Nixpkgs overlay for bleeding-edge XR/AR/VR packages";
 
@@ -9,18 +8,30 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" ];
+  outputs = { nixpkgs, self }: let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
 
-      imports = [
-        ./pkgs
-      ];
+    forSystem = system: fn: fn nixpkgs.legacyPackages.${system};
+    forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: forSystem system fn);
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [nvfetcher reuse];
-        };
+    mkSources = ps:
+      import ./_sources/generated.nix {
+        inherit (ps) fetchgit fetchurl fetchFromGitHub dockerTools;
       };
-    };
+
+    mkPackageSet = sources: prev:
+      nixpkgs.lib.genAttrs
+      (builtins.attrNames sources)
+      (pkg: prev.${pkg}.overrideAttrs (_: sources.${pkg}));
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [ nvfetcher reuse ];
+      };
+    });
+
+    packages = forAllSystems (pkgs: mkPackageSet (mkSources pkgs) pkgs);
+
+    overlays.default = final: prev: mkPackageSet (mkSources final) prev;
+  };
 }

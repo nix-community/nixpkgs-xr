b4d4f5e6f5f2a980df17a8ec44fb336c2e35ee84
seth <getchoo@tuta.io>
2024-01-01 14:24:10 -0500

    refactor: reuse overlay for packages

--- flake.nix
+++ flake.nix
@@ -13,16 +13,6 @@
 
     forSystem = system: fn: fn nixpkgs.legacyPackages.${system};
     forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: forSystem system fn);
-
-    mkSources = ps:
-      import ./_sources/generated.nix {
-        inherit (ps) fetchgit fetchurl fetchFromGitHub dockerTools;
-      };
-
-    mkPackageSet = sources: prev:
-      nixpkgs.lib.genAttrs
-      (builtins.attrNames sources)
-      (pkg: prev.${pkg}.overrideAttrs (_: sources.${pkg}));
   in {
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
@@ -30,8 +20,15 @@
       };
     });
 
-    packages = forAllSystems (pkgs: mkPackageSet (mkSources pkgs) pkgs);
+    packages = forAllSystems (pkgs: self.overlays.default pkgs pkgs);
 
-    overlays.default = final: prev: mkPackageSet (mkSources final) prev;
+    overlays.default = final: prev: let
+      sources = import ./_sources/generated.nix {
+        inherit (final) fetchgit fetchurl fetchFromGitHub dockerTools;
+      };
+
+      mkOverride = pkg: newAttrs: prev.${pkg}.overrideAttrs (_: newAttrs);
+    in
+      final.lib.mapAttrs mkOverride sources;
   };
 }

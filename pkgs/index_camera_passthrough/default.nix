c8de4915afd78096dcd498a1d34eb9071507a1fd
Sefa Eyeoglu <contact@scrumplex.net>
2024-01-12 19:40:47 +0100

    chore: fmt
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/index_camera_passthrough/default.nix
+++ pkgs/index_camera_passthrough/default.nix
@@ -16,34 +16,35 @@
   rustPlatform = makeRustPlatform {
     cargo = toolchain;
     rustc = toolchain;
-  }; 
-in rustPlatform.buildRustPackage {
-  pname = "index-camera-passthrough";
-  version = "0";
-
-  # src will be added by the source override
-  inherit cargoLock;
-
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-    rustPlatform.bindgenHook
-  ];
-
-  buildInputs = [
-    udev
-    vulkan-loader
-    openvr
-  ];
-
-  env.SHADERC_LIB_DIR = "${lib.getLib shaderc}/lib";
-
-  meta = with lib; {
-    description = "Experimental Valve Index camera passthrough for Linux";
-    homepage = "https://github.com/yshui/index_camera_passthrough";
-    license = licenses.mit;
-    maintainers = with maintainers; [Scrumplex];
-    mainProgram = "index_camera_passthrough";
-    platforms = platforms.linux;
   };
-}
+in
+  rustPlatform.buildRustPackage {
+    pname = "index-camera-passthrough";
+    version = "0";
+
+    # src will be added by the source override
+    inherit cargoLock;
+
+    nativeBuildInputs = [
+      cmake
+      pkg-config
+      rustPlatform.bindgenHook
+    ];
+
+    buildInputs = [
+      udev
+      vulkan-loader
+      openvr
+    ];
+
+    env.SHADERC_LIB_DIR = "${lib.getLib shaderc}/lib";
+
+    meta = with lib; {
+      description = "Experimental Valve Index camera passthrough for Linux";
+      homepage = "https://github.com/yshui/index_camera_passthrough";
+      license = licenses.mit;
+      maintainers = with maintainers; [Scrumplex];
+      mainProgram = "index_camera_passthrough";
+      platforms = platforms.linux;
+    };
+  }

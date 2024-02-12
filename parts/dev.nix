7f8313321fd5481117a730900c9b78de735b0ab4
Sefa Eyeoglu <contact@scrumplex.net>
2024-02-12 11:35:04 +0100

    refactor: implement excludes for flake formatter
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- parts/dev.nix
+++ parts/dev.nix
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2023 Sefa Eyeoglu <contact@scrumplex.net>
+# SPDX-FileCopyrightText: 2023 Naïm Favier <n@monade.li>
 #
 # SPDX-License-Identifier: MIT
 {
@@ -12,6 +13,36 @@
         ];
       };
 
-      formatter = pkgs.nixfmt-rfc-style;
+      formatter = pkgs.writeShellApplication {
+        name = "nixpkgs-xr-format";
+
+        runtimeInputs = [ pkgs.nixfmt-rfc-style ];
+        text = ''
+          nixfmt_args=()
+          while [[ $# -gt 0 ]]; do
+            case "$1" in
+              .)
+                # ignore "." parameter passed by nix fmt
+                shift
+                ;;
+              -c|--check)
+                nixfmt_args+=("$1")
+                shift
+                ;;
+              *)
+                echo "Unknown option $1" >&2
+                exit 1
+                ;;
+            esac
+          done
+
+          git_root=$(git rev-parse --show-toplevel)
+
+          git ls-files -z --cached --full-name -- ":!:_sources/" \
+            | grep -z '\.nix$' \
+            | sed -z "s|^|$git_root/|" \
+            | xargs -0 nixfmt "$@"
+        '';
+      };
     };
 }

1dff15c6ac28ff8e3c134fa6499ca6d8cb3988bf
Sefa Eyeoglu <contact@scrumplex.net>
2024-10-23 09:42:35 +0200

    wlx-overlay-s: add missing build inputs
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/overrides/wlx-overlay-s.nix
+++ pkgs/overrides/wlx-overlay-s.nix
@@ -3,9 +3,16 @@
 # SPDX-License-Identifier: MIT
 
 final: prev: {
-  wlx-overlay-s = prev.wlx-overlay-s.overrideAttrs {
+  wlx-overlay-s = prev.wlx-overlay-s.overrideAttrs (prevAttrs: {
     inherit (final.xrSources.wlx-overlay-s) pname version src;
 
     cargoDeps = final.rustPlatform.importCargoLock final.xrSources.wlx-overlay-s.cargoLock."Cargo.lock";
-  };
+
+    buildInputs =
+      prevAttrs.buildInputs or [ ]
+      ++ (with final; [
+        libGL
+        wayland
+      ]);
+  });
 }

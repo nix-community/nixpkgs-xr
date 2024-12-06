ea93611be488fa2d6608fc7b9cbd141f7fcd3a86
Sefa Eyeoglu <contact@scrumplex.net>
2024-12-06 18:32:12 +0100

    flake: nixfmt
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- parts/checks.nix
+++ parts/checks.nix
@@ -8,7 +8,9 @@
     let
       inherit (lib) filterAttrs mapAttrs' nameValuePair;
 
-      packages' = filterAttrs (_: pkg: !pkg.meta.broken && (lib.meta.availableOn pkgs.stdenv.hostPlatform pkg)) config.packages;
+      packages' = filterAttrs (
+        _: pkg: !pkg.meta.broken && (lib.meta.availableOn pkgs.stdenv.hostPlatform pkg)
+      ) config.packages;
 
       packageChecks = mapAttrs' (n: nameValuePair "package-${n}") packages';
       devShellChecks = mapAttrs' (n: nameValuePair "devShell-${n}") config.devShells;
--- pkgs/by-name/pr/proton-ge-rtsp-bin/package.nix
+++ pkgs/by-name/pr/proton-ge-rtsp-bin/package.nix
@@ -6,20 +6,27 @@
   lib,
   proton-ge-bin,
 }:
-proton-ge-bin.overrideAttrs (finalAttrs: _: {
-  pname = "proton-ge-rtsp-bin";
-  version = "GE-Proton9-20-rtsp16";
+proton-ge-bin.overrideAttrs (
+  finalAttrs: _: {
+    pname = "proton-ge-rtsp-bin";
+    version = "GE-Proton9-20-rtsp16";
 
-  src = fetchzip {
-    url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-    hash = "sha256-iq7oiDW5+51wzqYwASOGSV922c/pg1k29MdkIXlT34k=";
-  };
+    src = fetchzip {
+      url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
+      hash = "sha256-iq7oiDW5+51wzqYwASOGSV922c/pg1k29MdkIXlT34k=";
+    };
 
-  meta = {
-    inherit (proton-ge-bin.meta) description license platforms sourceProvenance;
-    homepage = "https://github.com/SpookySkeletons/proton-ge-rtsp";
-    maintainers = with lib.maintainers; [
-      Scrumplex
-    ];
-  };
-})
+    meta = {
+      inherit (proton-ge-bin.meta)
+        description
+        license
+        platforms
+        sourceProvenance
+        ;
+      homepage = "https://github.com/SpookySkeletons/proton-ge-rtsp";
+      maintainers = with lib.maintainers; [
+        Scrumplex
+      ];
+    };
+  }
+)

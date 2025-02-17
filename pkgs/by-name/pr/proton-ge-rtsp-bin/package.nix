8eafc0fa5ffe00fb92f3d4a5da7cc87cbeb4e017
Sefa Eyeoglu <contact@scrumplex.net>
2025-02-17 13:41:53 +0100

    proton-ge-rtsp-bin: fix override
    The derivation now accepts `steamDisplayName` as an input.
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/by-name/pr/proton-ge-rtsp-bin/package.nix
+++ pkgs/by-name/pr/proton-ge-rtsp-bin/package.nix
@@ -6,33 +6,30 @@
   lib,
   proton-ge-bin,
 }:
-proton-ge-bin.overrideAttrs (
-  finalAttrs: _: {
-    pname = "proton-ge-rtsp-bin";
-    version = "GE-Proton9-22-rtsp17";
+(proton-ge-bin.override {
+  steamDisplayName = "GE-Proton-rtsp";
+}).overrideAttrs
+  (
+    finalAttrs: _: {
+      pname = "proton-ge-rtsp-bin";
+      version = "GE-Proton9-22-rtsp17";
 
-    src = fetchzip {
-      url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-      hash = "sha256-1zj0y7E9JWrnPC9HllFXos33rsdAt3q+NamoxNTmHHM=";
-    };
+      src = fetchzip {
+        url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
+        hash = "sha256-1zj0y7E9JWrnPC9HllFXos33rsdAt3q+NamoxNTmHHM=";
+      };
 
-    postBuild = ''
-      # prevents steam from resetting compatability settings (in addition to upstream's modifications)
-      sed -i -r 's|GE-Proton-rtsp[0-9]*|GE-Proton-rtsp|' $steamcompattool/compatibilitytool.vdf
-      sed -i -r 's|GE-Proton-rtsp[0-9]*|GE-Proton-rtsp|' $steamcompattool/proton
-    '';
-
-    meta = {
-      inherit (proton-ge-bin.meta)
-        description
-        license
-        platforms
-        sourceProvenance
-        ;
-      homepage = "https://github.com/SpookySkeletons/proton-ge-rtsp";
-      maintainers = with lib.maintainers; [
-        Scrumplex
-      ];
-    };
-  }
-)
+      meta = {
+        inherit (proton-ge-bin.meta)
+          description
+          license
+          platforms
+          sourceProvenance
+          ;
+        homepage = "https://github.com/SpookySkeletons/proton-ge-rtsp";
+        maintainers = with lib.maintainers; [
+          Scrumplex
+        ];
+      };
+    }
+  )

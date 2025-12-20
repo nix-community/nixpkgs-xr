c1d09b688118b640fa1729265933ab55c33d7d0c
Red_M <1468433+Red-M@users.noreply.github.com>
2025-12-20 11:36:45 +1000

    Update pkgs/by-name/wa/wayvr-dashboard/package.nix
    Co-authored-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/by-name/wa/wayvr-dashboard/package.nix
+++ pkgs/by-name/wa/wayvr-dashboard/package.nix
@@ -46,10 +46,8 @@ rustPlatform.buildRustPackage {
   cargoLock = xrSources.wayvr-dashboard.cargoLock."${cargoRoot}/Cargo.lock";
 
   npmDeps = importNpmLock {
-    package = builtins.fromJSON (builtins.readFile xrSources.wayvr-dashboard.extract."package.json");
-    packageLock = builtins.fromJSON (
-      builtins.readFile xrSources.wayvr-dashboard.extract."package-lock.json"
-    );
+    package = lib.importJSON xrSources.wayvr-dashboard.extract."package.json";
+    packageLock = lib.importJSON xrSources.wayvr-dashboard.extract."package-lock.json";
   };
 
   nativeBuildInputs = [

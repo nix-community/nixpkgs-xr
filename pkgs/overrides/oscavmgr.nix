d28be2ee4a9615ba974e4b73d6d50d421ed427aa
Sefa Eyeoglu <contact@scrumplex.net>
2026-03-27 23:32:54 +0100

    fix(oscavmgr): fix Rust deps patch
    Effectively reverts
    https://github.com/NixOS/nixpkgs/commit/796ce16ffc243c08c8031f078a95fdf37ac2dc13
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/overrides/oscavmgr.nix
+++ pkgs/overrides/oscavmgr.nix
@@ -6,6 +6,14 @@ final: prev: {
   oscavmgr = prev.oscavmgr.overrideAttrs (prevAttrs: {
     inherit (final.xrSources.oscavmgr) pname version src;
 
+    postPatch = ''
+      alvr_session=$(echo $cargoDepsCopy/alvr_session-*/)
+      substituteInPlace "$alvr_session/build.rs" \
+        --replace-fail \
+          'alvr_filesystem::workspace_dir().join("openvr/headers/openvr_driver.h")' \
+          '"${final.openvr}/include/openvr/openvr_driver.h"'
+    '';
+
     cargoDeps = final.rustPlatform.importCargoLock final.xrSources.oscavmgr.cargoLock."Cargo.lock";
 
     doInstallCheck = false; # sha1 doesn't match version of tool

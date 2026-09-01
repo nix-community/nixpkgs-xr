16761cc871939fe0a2d4b1e6d9d9884d0bf83f06
ACMDF <233685277+acmdf@users.noreply.github.com>
2026-09-02 08:18:30 +1000

    proton-ge-rtsp-bin: use new override interface

--- pkgs/by-name/pr/proton-ge-rtsp-bin/package.nix
+++ pkgs/by-name/pr/proton-ge-rtsp-bin/package.nix
@@ -7,41 +7,38 @@
   fetchzip,
   lib,
   proton-ge-bin,
+  stdenvNoCC,
 }:
-let
-  steamDisplayName = "GE-Proton-rtsp";
-in
-(proton-ge-bin.override {
-  inherit steamDisplayName;
-}).overrideAttrs
-  (
-    finalAttrs: _: {
-      pname = "proton-ge-rtsp-bin";
-      version = "GE-Proton10-33-rtsp23-4";
+proton-ge-bin.overrideAttrs (
+  finalAttrs: prevAttrs: {
+    pname = "proton-ge-rtsp-bin";
+    version = "GE-Proton10-33-rtsp23-4";
 
+    inherit (finalAttrs.passthru.variants.${stdenvNoCC.hostPlatform.system}) src toolName;
+
+    steamDisplayName = "GE-Proton-rtsp";
+
+    passthru.variants."x86_64-linux" = {
+      toolName = finalAttrs.version;
       src = fetchzip {
         url = "https://github.com/SpookySkeletons/proton-ge-rtsp/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
         hash = "sha256-sP+xNPbeI1jbs081QvFmj48A/yG6IC9ZPZRvGkFZnX0=";
       };
+    };
 
-      preFixup = ''
-        substituteInPlace "$steamcompattool/compatibilitytool.vdf" \
-          --replace-fail "${finalAttrs.version}" "${steamDisplayName}"
-      '';
-
-      meta = {
-        inherit (proton-ge-bin.meta)
-          description
-          license
-          platforms
-          sourceProvenance
-          ;
-        homepage = "https://github.com/SpookySkeletons/proton-ge-rtsp";
-        maintainers = with lib.maintainers; [
-          Scrumplex
-          RTUnreal
-          coolGi
-        ];
-      };
-    }
-  )
+    meta = {
+      inherit (prevAttrs.meta)
+        description
+        license
+        platforms
+        sourceProvenance
+        ;
+      homepage = "https://github.com/SpookySkeletons/proton-ge-rtsp";
+      maintainers = with lib.maintainers; [
+        Scrumplex
+        RTUnreal
+        coolGi
+      ];
+    };
+  }
+)
